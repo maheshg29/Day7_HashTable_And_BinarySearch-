87e53df4e5ef9756170dd34135822a3b90c3724b
mahesh <gangurdemahesh9@gmail.com>
2023-03-28 19:05:27 +0530

    [mahesh]add program file for Find freq of word in given Sentence

---
+++ Day7-HashTable-And-BinarySearch/HashTableOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_HashTable_And_BinarySearch
+{
+    class HashTableOperations
+    {
+        public void FrequencyOfWord(string inputString)
+        {
+            Hashtable wordFrequency = new Hashtable();
+            string[] words = inputString.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (wordFrequency.ContainsKey(word))
+                {
+                    int frequency = (int)wordFrequency[word];
+                    wordFrequency[word] = frequency + 1;
+
+                }
+                else
+                {
+                    wordFrequency.Add(word, 1);
+                }
+            }
+
+            foreach (DictionaryEntry entry in wordFrequency)
+            {
+                Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+            }
+        }
+    }
+}
--- Day7-HashTable-And-BinarySearch/Program.cs
+++ Day7-HashTable-And-BinarySearch/Program.cs
@@ -11,6 +11,17 @@ namespace Day7_HashTable_And_BinarySearch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 7 HashTable and Binary Search Practice Problem");
+            Console.WriteLine("Select one Option for \n 1. Find frequency of words in a sentence");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    HashTableOperations findFrequencyOfWord=new HashTableOperations();
+                    findFrequencyOfWord.FrequencyOfWord("To be or not to be");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
