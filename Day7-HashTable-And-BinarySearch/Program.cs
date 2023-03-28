d5b156b253b45fac4b4a3e53b7e0ce51a1955346
mahesh <gangurdemahesh9@gmail.com>
2023-03-28 18:21:37 +0530

    [mahesh]add program file for welcome greting message

---
+++ Day7-HashTable-And-BinarySearch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_HashTable_And_BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 7 HashTable and Binary Search Practice Problem");
+        }
+    }
+}
